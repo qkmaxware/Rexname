062d23e6ba4aafc5eb7571dc0a609d434ec40b42
Colin Halseth <qkmaxware@gmail.com>
2024-03-10 11:43:58 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+class Program {
+
+    /*/// <summary>
+    /// Rename all files by adding a prefix to the filename
+    /// </summary>
+    /// <param name="prefix">prefix to add</param>
+    /// <param name="dry">mark this as a dry run or not</param>
+    public static void Main(string prefix, bool dry = false) {
+        var Current = new DirectoryInfo(Environment.CurrentDirectory);
+        foreach (var file in Current.GetFiles("*", SearchOption.AllDirectories)) {
+            var before = file.Name;
+            var after = prefix + file.Name;
+            var parent = file.Directory;
+            if (parent is null)
+                continue;
+            
+            if (dry) {
+                var root = Path.GetRelativePath(Current.FullName, parent.FullName);
+                var root_b4 = Path.Combine(root, before);
+                var root_afr = Path.Combine(root, after);
+                Console.WriteLine($"{root_b4} -> {root_afr}");
+                continue;
+            }
+            file.MoveTo(Path.Join(parent.FullName, after));
+        }
+    }*/
+
+    /// <summary>
+    /// Rename all files in the directory matching the given pattern. Can use match groups in the replacement name
+    /// </summary>
+    /// <param name="pattern">RegEx matching pattern</param>
+    /// <param name="name">Replacement name pattern</param>
+    /// <param name="dry">mark this as a dry run or not</param>
+    public static void Main(string pattern, string name, bool dry = false) {
+        var regex = new Regex(pattern);
+        var variable = new Regex(@"\%(?<index>\d+)|(?<!{){\s*(?<name>\w+)\s*}");
+        var Current = new DirectoryInfo(Environment.CurrentDirectory);
+        foreach (var file in Current.GetFiles("*", SearchOption.AllDirectories)) {
+            var before = file.Name;
+            var match = regex.Match(file.Name);
+            if (!match.Success)
+                continue;
+            var after = variable.Replace(name, (replacement) => {
+                if (!replacement.Success)
+                    return string.Empty;
+                if (replacement.Groups["index"].Success) {
+                    var index = int.Parse(replacement.Groups["index"].Value);
+                    return match.Groups[index].Value;
+                } else if (replacement.Groups["name"].Success) {
+                    return match.Groups[replacement.Groups["name"].Value].Value;
+                } else {
+                    return string.Empty;
+                }
+            });
+            var parent = file.Directory;
+            if (parent is null)
+                continue;
+
+            var root = Path.GetRelativePath(Current.FullName, parent.FullName);
+            var root_b4 = Path.Combine(root, before);
+            var root_afr = Path.Combine(root, after);
+            Console.WriteLine($"{root_b4} -> {root_afr}");
+            if (dry) {
+                continue;
+            }
+            file.MoveTo(Path.Join(parent.FullName, after));
+        }
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,44 @@
+# Rexname
+A tool for using RegEx to bulk rename files.
+
+## Installation
+1. Clone or Download repo 
+2. Using dotnet 8 or newer run the following commands
+   1. dotnet pack rexname.csproj
+   2. dotnet tool install --global --add-source ./nupkg rexname
+
+## Uninstall
+1. Using dotnet cli run the following commands
+   1. dotnet tool uninstall rexname
+
+## Usage
+The rexname tool runs on the command line and you can access it's help information with the following command
+```
+> rexname --help
+
+Description:
+  Rename all files in the directory matching the given pattern. Can use match groups in the replacement name
+
+Usage:
+  rexname [options]
+
+Options:
+  --pattern <pattern>  RegEx matching pattern
+  --name <name>        Replacement name pattern
+  --dry                mark this as a dry run or not [default: False]
+  --version            Show version information
+  -?, -h, --help       Show help and usage information
+```
+
+First specify a file selection pattern. Only files matching this pattern will be renamed. For instance if we wanted to rename all mp4 video files one possible pattern would be `(?<name>.*)\.mp4$`. Then you need to specify a renaming template something like maybe we want to append ".old" to the end of each file then the template would be `{name}.mp4.old`. Arguments in {} will expand to the value of capture groups from the original matching pattern. 
+
+The combined CLI instruction would be 
+```
+> rexname --pattern "(?<name>.*)\.mp4$" --name "{name}.mp4.old"
+```
+
+An optional parameter `--dry true` allows one to see the changes that would occur as a result of renaming without actually renaming the files. Good for testing that your pattern or name template are correct without causing permanent damage. 
+
+```
+> rexname --pattern "(?<name>.*)\.mp4$" --name "{name}.mp4.old" --dry true
+```
